2f33d7acdfa618448cb28b4b4a3db512d0704efd
SwartzJonah <swartzjonah@gmail.com>
2023-03-20 15:50:28 -0400

    recursive fib function

--- assignment1/main.js
+++ assignment1/main.js
@@ -1,4 +1,4 @@
-function iterationFib(n){
+function fibs(n){
     let array = []
     let num = 0;
     let temp = 0;
@@ -13,4 +13,22 @@ function iterationFib(n){
     console.log(array);
 }
 
-iterationFib(8)
+fibs(8)
+
+function fibsRec(n){
+   // solving without array
+    // if (n < 2){
+    //     return n;
+    // } else{
+    //     return (fibsRec(n-1) + fibsRec(n-2))
+    // }
+ 
+    //solving with the array
+ if (n === 1){
+        return[0];
+    } else if (n === 2){
+        return [0,1];
+    }return [...fibsRec(n-1), fibsRec(n-1)[n-2] + fibsRec(n-1)[n-3]]
+}
+let answer=fibsRec(8);
+console.log(answer)
